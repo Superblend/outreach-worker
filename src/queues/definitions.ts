b53a164dca63ce21a484186f03059e3887171b48
Taha <taha@superblend.ai>
2026-03-30 14:47:25 +0200

    Replace colons with dashes in BullMQ queue names

--- src/queues/definitions.ts
+++ src/queues/definitions.ts
@@ -8,13 +8,13 @@ export const connection = new IORedis(config.redis.url, {
 });
 
 // Main queues
-export const executionQueue = new Queue('outreach:executions', { connection });
-export const batchQueue = new Queue('outreach:batches', { connection });
-export const scannerQueue = new Queue('outreach:scanner', { connection });
-export const recoveryQueue = new Queue('outreach:recovery', { connection });
+export const executionQueue = new Queue('outreach-executions', { connection });
+export const batchQueue = new Queue('outreach-batches', { connection });
+export const scannerQueue = new Queue('outreach-scanner', { connection });
+export const recoveryQueue = new Queue('outreach-recovery', { connection });
 
 // Queue events for monitoring
-export const executionEvents = new QueueEvents('outreach:executions', { connection });
-export const batchEvents = new QueueEvents('outreach:batches', { connection });
+export const executionEvents = new QueueEvents('outreach-executions', { connection });
+export const batchEvents = new QueueEvents('outreach-batches', { connection });
 
 export const allQueues = [executionQueue, batchQueue, scannerQueue, recoveryQueue];
